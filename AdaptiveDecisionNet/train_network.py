6031193bd06485d7cefe4e01a0b5db25e84c4872
HustAIsGroup <jizhang@hust.edu.cn>
2020-04-22 09:48:49 +0800

    Revert "AlphaJoin"
    This reverts commit 94e7cec2d01ec65b4657acfba8783a8dc23b7606.

--- AdaptiveDecisionNet/train_network.py
+++
@@ -1,9 +0,0 @@
-from arguments import get_args
-from supervised import supervised
-
-if __name__ == '__main__':
-    args = get_args()
-
-    trainer = supervised(args)
-    trainer.supervised()
-    trainer.test_network()
--- tools/dropCache.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-/bin/systemctl restart  postgresql-10
-echo 3 > /proc/sys/vm/drop_caches
-echo  finish
-
